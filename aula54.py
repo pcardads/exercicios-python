9fbc5d001215a5b29659eb7d2ab701298afa0bd1
pcardads <paulocardosoads@outlook.com>
2024-11-18 23:07:33 -0300

    Lista de compras

---
+++ aula54.py
@@ -0,0 +1,38 @@
+#lista de compras
+import os
+
+lista = []
+while True:
+    print('Selecione uma opção: ')
+    opcao = input('[i]nserir [a]pagar [l]istar: ')
+
+    if opcao == 'i':
+        os.system('cls')
+        valor = input('Valor: ')
+        lista.append(valor)
+
+    elif opcao == 'a':
+        indice_str = input(
+            'Escolha o índice para apagar: '
+        )
+
+        try: 
+            indice = int(indice_str)
+            del lista[indice]
+        except ValueError:
+            print('Por favor, digite um numero inteiro.')
+
+        except IndexError:
+            print('Índice inesistente.')
+
+    elif opcao == 'l':
+        os.system('cls')
+
+        if len(lista) == 0:
+            print('Nada para listar')
+
+        for i, valor in enumerate(lista):
+            print(i, valor)
+
+    else:
+        print('Por favor, escolha apenas entre as letras i, a ou l')
